854d9d0896596c66f05ee0a5f457552257deb64d
Ben <zhangbenber@qq.com>
2014-11-19 19:15:28 +0800

    Original version

---
+++ gen/com/benber/isurvey/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.benber.isurvey;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/benber/isurvey/LaunchActivity.java
@@ -0,0 +1,74 @@
+package com.benber.isurvey;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class LaunchActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Go full screen
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(R.layout.activity_launch);
+
+		// Fill the login panel placeholder
+		if (savedInstanceState == null)
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.login, new LoginFragment()).commit();
+
+		// Wait to display the panel
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Animation animation = AnimationUtils.loadAnimation(
+						LaunchActivity.this, R.anim.login_show);
+				findViewById(R.id.login).setVisibility(View.VISIBLE);
+				findViewById(R.id.login).startAnimation(animation);
+				animation = AnimationUtils.loadAnimation(LaunchActivity.this,
+						R.anim.welcome_show);
+				findViewById(R.id.welcome).setVisibility(View.VISIBLE);
+				findViewById(R.id.welcome).startAnimation(animation);
+				animation = AnimationUtils.loadAnimation(LaunchActivity.this,
+						R.anim.logo_up);
+				findViewById(R.id.logo).startAnimation(animation);
+			}
+		}, 2500);
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Disable the Menu button, which may be a bug when in FULLSCREEN mode
+		// TODO The reason is to be found...
+		if (keyCode == KeyEvent.KEYCODE_MENU)
+			return true;
+		return super.onKeyDown(keyCode, event);
+	}
+
+	public static class LoginFragment extends Fragment {
+
+		public LoginFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_login,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
